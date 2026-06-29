c3d339e8ee0bb6a5a459483e72a02f491551a923
kcafernandes <74519872+kcafernandes@users.noreply.github.com>
2026-06-29 14:58:42 -0700

    add receiver module placeholder

---
+++ src/edgevision/receiver.py
@@ -0,0 +1,8 @@
+"""
+edgeVision receiver module
+--> runs on Jetson Orin Nano B
+
+this file will receive hazard alerts from the detector node,
+prioritize urgent warnings, and convert them into audio/TTS feedback.
+
+"""
