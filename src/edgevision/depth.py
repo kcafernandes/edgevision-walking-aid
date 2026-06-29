809b77e5cccabc928553e74a94da8986efce933f
kcafernandes <74519872+kcafernandes@users.noreply.github.com>
2026-06-29 15:12:18 -0700

    add depth module placeholder

---
+++ src/edgevision/depth.py
@@ -0,0 +1,7 @@
+"""
+depth estimation utilities for edgevision
+
+this file will contain preprocessing and TensorRT inference code
+for running Depth Anything V2 metric depth estimation on jetson
+
+"""
